663f91dbe68fcfa7d56225ee427d1c1a4501ef39
Pranav Balaji <pranavbalaji9276@gmail.com>
2026-06-05 22:30:54 -0400

    updates

---
+++ switching.sh
@@ -0,0 +1,71 @@
+  remotes/origin/codex/session-history-guest-edit
+  remotes/origin/fix-dashboard-ui
+  remotes/origin/kushtesting
+  remotes/origin/main
+  remotes/origin/pr-8
+  remotes/origin/redesign-auth-page
+  remotes/origin/testPranav
+  remotes/origin/testin
+  remotes/origin/testing111111
+pranavbalaji@Mac chipCount % git status
+On branch codex/remove-create-table-guests
+Your branch is up to date with 'origin/codex/remove-create-table-guests'.
+
+nothing to commit, working tree clean
+pranavbalaji@Mac chipCount % git switch ChipCountMobileNew
+Updating files: 100% (29284/29284), done.
+Switched to branch 'ChipCountMobileNew'
+Your branch is up to date with 'origin/ChipCountMobileNew'.
+pranavbalaji@Mac chipCount % ls
+ChipCount		Package.swift		Sources
+ChipCount.xcodeproj	project.yml		Tests
+ios			README.md
+pranavbalaji@Mac chipCount % clear
+
+pranavbalaji@Mac chipCount % open *.xcodeproj 
+pranavbalaji@Mac chipCount % git status
+On branch ChipCountMobileNew
+Your branch is up to date with 'origin/ChipCountMobileNew'.
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+	modified:   ChipCount.xcodeproj/project.xcworkspace/xcuserdata/pranavbalaji.xcuserdatad/UserInterfaceState.xcuserstate
+
+no changes added to commit (use "git add" and/or "git commit -a")
+pranavbalaji@Mac chipCount % nano switching.sh
+pranavbalaji@Mac chipCount % nano switching.sh
+
+
+
+
+
+
+
+
+
+
+  UW PICO 5.09                   File: switching.sh                   Modified  
+
+hello
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+^G Get Help  ^O WriteOut  ^R Read File ^Y Prev Pg   ^K Cut Text  ^C Cur Pos   
+^X Exit      ^J Justify   ^W Where is  ^V Next Pg   ^U UnCut Text^T To Spell  
